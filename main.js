d470a93d458bd3e68aec4e044acdf1cfac982c18
Paulo Henrique Silva <91227069+phdevlpr@users.noreply.github.com>
2021-10-07 18:18:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// DOM - Document Object Model
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-//Quando clicar em um item do menu, esconder
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -17,23 +17,21 @@ for (const link of links) {
   })
 }
 
-//mudar o header da página quando der scroll
-
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
-    //scroll é maior que a altura do header
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
-    //menor que a altura do header
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
 }
 
-// Testimonials carousel slider swiper
-
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -44,16 +42,16 @@ const swiper = new Swiper('.swiper-container', {
   breakpoints: {
     767: {
       slidesPerView: 2,
-      serWrapperSize: true
+      setWrapperSize: true
     }
   }
 })
 
-// Scroll Review: Mostrar elementos
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 100,
   reset: true
 })
 
@@ -68,8 +66,7 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
-//Botão voltar para o topo
-
+/* Botão voltar para o topo */
 const backToTopButton = document.querySelector('.back-to-top')
 
 function backToTop() {
@@ -80,7 +77,7 @@ function backToTop() {
   }
 }
 
-//Menu ativo conforme a seção visível na página
+/* Menu ativo conforme a seção visível na página */
 const sections = document.querySelectorAll('main section[id]')
 function activateMenuAtCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
@@ -105,8 +102,7 @@ function activateMenuAtCurrentSection() {
   }
 }
 
-//When Scroll
-
+/* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
